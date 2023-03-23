ccb24dddb81044f44070e83c6c5833174af74ac5
Zachary Zollman <85812228+zacharyzollman@users.noreply.github.com>
2023-03-22 18:02:25 -0700

    use Redis instead of InMemoryLRUCache

--- src/bootstrap/loaders/apollo.ts
+++ src/bootstrap/loaders/apollo.ts
@@ -1,12 +1,18 @@
 import { ApolloServer } from "@apollo/server";
 import { buildSchema } from "../../utils/buildSchema";
+import Keyv from "keyv";
+import { KeyvAdapter } from "@apollo/utils.keyvadapter";
 import responseCachePlugin from '@apollo/server-plugin-response-cache';
+import { ErrorsAreMissesCache } from "@apollo/utils.keyvaluecache";
 
 export default async () => {
   const schema = buildSchema();
+  const redisCache = new KeyvAdapter(new Keyv("redis://localhost:6379"))
+  const faultTolerantCache = new ErrorsAreMissesCache(redisCache)
 
   return new ApolloServer({
     schema,
+    cache: faultTolerantCache,
     plugins: [responseCachePlugin()],
   });
 };
